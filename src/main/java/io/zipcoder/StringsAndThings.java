33ea930f1a8ec3d2943474976e5c6b2ac2cbd4e1
surabhi <surabhi.das@outlook.com>
2023-07-04 20:02:18 -0400

    first time all my tests ran woo

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -15,7 +15,26 @@ public class StringsAndThings {
      *           countYZ("day fyyyz"); // Should return 2
      */
     public Integer countYZ(String input){
-        return null;
+
+        int count = 0;
+        // \s+ reconizgizes spaces as a delimiter
+        String [] word = input.split("\\s+");
+
+        for (int i=0; i<word.length; i++) {
+            //iterating thru each of the words
+            String splitWord = word[i];
+            //get the last character of split words
+            char lastChar = splitWord.charAt(splitWord.length()-1);
+
+            if ((lastChar) == 'y' || (lastChar) == 'z') {
+                if(Character.isLetter(lastChar)) {
+                    count ++;
+                }
+            }
+
+
+        }
+        return count;
     }
 
     /**
@@ -28,7 +47,57 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+        int removeLength = remove.length ();
+        int baseLength = base.length();
+
+        /*starts at index 0 and ends at base length -remove length = remaining base
+        which potentially matches with remove string */
+        for (int i=0;i<=baseLength-removeLength; ) {
+
+            /*in the substring... i= where the beginning of base and remove match and
+             i+remove is taking that index where it matches and adding it to length of
+             remove length ...resulting in a substring that specifies where remove length
+             is in base length....
+
+             for example:
+
+             in base string "Hello there" and in the remove string "llo"
+             after starting at 0 and iteratng ...i=2 bc "llo" starts at index 2
+             now if u add 2(i) + 3(the length of the remove string) = 5... 5 is
+             the end of substring...
+
+             so "llo" is base.substring(2,5)
+
+
+             */
+            if(base.substring(i,i+removeLength).equalsIgnoreCase(remove)) {
+                /* removeLength is getting added to i if substring and remove string match
+
+                During the iteration when i is 2, we find a match between the substring "llo" (extracted from the base string)
+                and the remove string "llo".
+
+                Instead of continuing the loop from index 3, we want to skip over the matched substring and continue the search.
+                So, we increment i by removeLength, which is 3(llo) in this case.
+                */
+
+                i += removeLength;
+            } else {
+                //appending the leftover non matched chars to sb
+                sb.append(base.charAt(i));
+                /*incrementing by one to ensure loop loop goes thru base string and either matches
+                or appends*/
+                i++;
+
+            }
+        }
+        /*final appending of the remaining characters from the base string, starting from the position
+         after the last potential match of the remove string to the rest of base string
+        adds 1 to start after the last position where a match could potentially occur.
+        */
+        sb.append(base.substring(baseLength-removeLength+1));
+        return sb.toString();
     }
 
     /**
@@ -39,8 +108,45 @@ public class StringsAndThings {
      *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
      *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
-    public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
+    public Boolean containsEqualNumberOfIsAndNot(String input) {
+
+        boolean equalCounts = true;
+        //keep track of starting position to look for "is"
+        int start =0;
+        //keeps track of the occurances of "is"
+        int count =0;
+
+        //while loop to count for "is"
+        //i think -1 works for start bc it will start at 0
+        //will keep checking for is as long as "start" is not -1
+        while (start!= -1) {
+            //.indexOf find index where is starts
+            start =input.indexOf("is", start);
+                if (start != -1) {
+                    count++;
+                    // 2 bc length of 'is' is 2
+                    start += 2;
+                }
+
+             }
+        int countForIs =count;
+            //same while loop again for "not"
+            start = 0;
+            count =0;
+
+            while (start != -1) {
+                start = input.indexOf("not",start);
+                    if (start != -1) {
+                        count++;
+                        //3 bc not is 3 letters
+                        start+=3;
+                    }
+            }
+            int countForNot = count;
+
+            equalCounts = (countForIs == countForNot);
+
+            return equalCounts;
     }
 
     /**
@@ -50,8 +156,31 @@ public class StringsAndThings {
      *           gHappy("xxgxx") // Should return  false
      *           gHappy("xxggyygxx") // Should return  false
      */
-    public Boolean gIsHappy(String input){
-        return null;
+    public Boolean gIsHappy(String input) {
+
+        for (int i = 0; i < input.length(); i++) {
+            //with iteration, if statement checks if any char is 'g'
+            if (input.charAt(i) == 'g') {
+                //another if statement to check chars on the left and right
+                //i>0 ensures we dont go out of bounds incase g is the first letter
+                //i<input.length()-1 is incase g is the last letter
+                if ((i > 0 && input.charAt(i - 1) == 'g') || (i < input.length() - 1 && input.charAt(i + 1) == 'g')) {
+                    //jumps out of if statement and does not go to the else
+
+                    return true;
+
+                } else if ((i > 0 && input.charAt(i - 1) != 'g') || (i < input.length() - 1 && input.charAt(i + 1) != 'g')) {
+
+                    return false;
+                } else {
+                    return false;
+
+                }
+
+            }
+
+        }
+        return false;
     }
 
 
@@ -63,6 +192,22 @@ public class StringsAndThings {
      *            countTriple("a") // Should return 0
      */
     public Integer countTriple(String input){
-        return null;
+
+        int count =0;
+        int letters =input.length();
+        /*-2 ensures there are atleast two characters remaining after the current position to evaluate
+        triple letter*/
+        for (int i=0; i < letters-2;i++) {
+            //char currentLetter =input.charAt(i);
+            //checking the next two letters on the right
+            if (input.charAt(i)==input.charAt(i+1) && input.charAt(i)==input.charAt(i+2)) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
+
+
+
